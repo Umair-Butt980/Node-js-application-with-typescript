35f1f5816cb03a79d7db830b4fea1bd9e1a8c401
UmairButt <umairamjadbutt1@gmail.com>
2023-01-04 23:19:24 +0500

    implemnted multer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 const express = require('express');
+const multer= require("multer");
+
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        cb(null, './uploads/')
+    },
+    filename:function(req,file,cb){
+        cb(null, file.originalname)
+    }
+});
+
+const fileFilter = (req,file,cb)=>{
+    if(file.mimetype=="image/jpeg" || file.mimetype=='image/png'){
+        cb(null,true)
+    }else{
+        cb(null,false)
+    }
+};
+
+const upload = multer({storage:storage,fileFilter:fileFilter});
 const app = express();
 
 
-app.post('/uploadFile', (req,res)=>{
-    console.log(req.body,"<=====>")
+
+app.post('/uploadFile', upload.single('image'), (req,res) => {
+    console.log(req.file,"<=====>")
 })
 
 app.listen(3000, ()=>{
