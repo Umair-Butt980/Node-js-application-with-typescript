c500cb2201a43339195e07528d21bde7151581d1
UmairButt <umairamjadbutt1@gmail.com>
2023-01-05 00:29:19 +0500

    Code in working condition

--- src/controller/uploader.ts
+++ src/controller/uploader.ts
@@ -1,38 +1,9 @@
 var streamingS3 = require('streaming-s3'),
 fs = require('fs');
 
-var fStream = fs.createReadStream(__dirname + '/video.mp4');
 
-let accessKey = "AKIAZ2OL7XHFDX2CFBFV";
-let secretKey= "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"
+function uploadStream(file) {
+    console.log(file,"This is inside te controller")
+    
+}
 
-var uploader = new streamingS3(fStream, {accessKeyId: accessKey, secretAccessKey: secretKey},
-{
-    Bucket: 'file-mover-prototype-bucket',
-    Key: 'video.mp4',
-    ContentType: 'video/mp4'
-  }
-);
-
-uploader.on('data', function(bytesRead) {
-  console.log(bytesRead, ' bytes read.');
-});
-
-uploader.on('part', function(number) {
-  console.log('Part ', number, ' uploaded.');
-});
-
-// All parts uploaded, but upload not yet acknowledged.
-uploader.on('uploaded', function(stats) {
-  console.log('Upload stats: ', stats);
-});
-
-uploader.on('finished', function(resp, stats) {
-  console.log('Upload finished: ', resp);
-});
-
-uploader.on('error', function(e) {
-  console.log('Upload error: ', e);
-});
-
-uploader.begin();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 const express = require('express');
 const multer= require("multer");
+var streamingS3 = require('streaming-s3'),
+fs = require('fs');
+
 
 const storage = multer.diskStorage({
     destination:function(req, file, cb){
@@ -23,13 +26,53 @@ const app = express();
 
 
 
-app.post('/uploadFile', upload.single('image'), (req,res) => {
-    console.log(req.file,"<=====>")
-})
-
-app.listen(3000, ()=>{
-    console.log("Listening to the port 3000...")
-})
-
-
-
+app.post('/uploadFile', upload.single('image'), (req, res) => {
+    try {
+        console.log(req.file,"<=====>")
+        var fStream = fs.createReadStream('./uploads/' + 'connect.png');
+        
+        let accessKey = "AKIAZ2OL7XHFDX2CFBFV";
+        let secretKey= "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"
+        
+        var uploader = new streamingS3(fStream, {accessKeyId: accessKey, secretAccessKey: secretKey},
+            {
+                Bucket: 'file-mover-prototype-bucket',
+                Key: 'image',
+                ContentType: 'image/png'
+            }
+            );
+            
+            uploader.on('data', function(bytesRead) {
+                console.log(bytesRead, ' bytes read.');
+            });
+            
+            uploader.on('part', function(number) {
+                console.log('Part ', number, ' uploaded.');
+            });
+            
+            // All parts uploaded, but upload not yet acknowledged.
+            uploader.on('uploaded', function(stats) {
+                console.log('Upload stats: ', stats);
+            });
+            
+            uploader.on('finished', function(resp, stats) {
+                console.log('Upload finished: ', resp);
+            });
+            
+            uploader.on('error', function(e) {
+                console.log('Upload error: ', e);
+            });
+            
+            uploader.begin();
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    
+    app.listen(3000, ()=>{
+        console.log("Listening to the port 3000...")
+    })
+    
+    
+    
+    
